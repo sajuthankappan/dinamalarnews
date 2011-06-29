fa5f630f2b21f6c07a4bbb3efe271159d015ae38
Saju Thankappan <sajuthankappan@gmail.com>
2011-06-29 06:34:32 +0000

    Feeds are now defined using an array

--- js/app.js
+++ js/app.js
@@ -3,17 +3,27 @@ google.load("jquery", "1.4.2");
 google.load("jqueryui", "1.8.13");
 
 var dmFeeds=[]; // regular array (add an optional integer
-dmFeeds[0]="http://feeds.feedburner.com/dinamalar/Front_page_news";       // argument to control array's size)
-dmFeeds[1]="http://rss.dinamalar.com/?cat=ara1";
-dmFeeds[2]="http://rss.dinamalar.com/sportsnews.asp";
+dmFeeds[0] = [];
+dmFeeds[0][0]="முதல் பக்க செய்திகள்";
+dmFeeds[0][1]="http://feeds.feedburner.com/dinamalar/Front_page_news";
+dmFeeds[1] = [];
+dmFeeds[1][0]="அரசியல் செய்திகள";
+dmFeeds[1][1]="http://rss.dinamalar.com/?cat=ara1";
+dmFeeds[2] = [];
+dmFeeds[2][0]="பொது செய்தி";
+dmFeeds[2][1]="http://rss.dinamalar.com/?cat=pot1";
+dmFeeds[3] = [];
+dmFeeds[3][0]="விளையாட்டு";
+dmFeeds[3][1]="http://rss.dinamalar.com/sportsnews.asp";
 
 function initialize() {
-    var $tabs = $( "#tabs" ).tabs();
     var $maxitems = $("#maxitems").slider({ min:5, max: 20, value: 5});
     var lastUsedFeedId = 0;
     var maxItemsToShow = 5;
     
     loadSettings();
+    loadTabs();
+    var $tabs = $( "#tabs" ).tabs();
     
     $tabs.tabs('select',lastUsedFeedId);
     $("#maxitemstext").text("செய்தி எண்ணிக்கை: " + maxItemsToShow);
@@ -51,7 +61,7 @@ function initialize() {
     function loadFeed(feedId) {
         $("#feed").html("பதிவிறக்கம்...");
         
-        var feed = new google.feeds.Feed(dmFeeds[feedId]);
+        var feed = new google.feeds.Feed(dmFeeds[feedId][1]);
         
         feed.setNumEntries(maxItemsToShow);
         feed.load(function(result) {
@@ -69,8 +79,19 @@ function initialize() {
         });
     }
     
+    function loadTabs() {
+        var feedHtml = '<ul>';
+        $.each(dmFeeds,
+            function( intIndex, objValue ){
+                feedHtml = feedHtml + '<li><a href="#feed">' + objValue[0] + '</a></li>';
+            }
+        );
+        feedHtml = feedHtml + '</ul><div id="feed"></div>';
+        $("#tabs").append(feedHtml);
+    }
+    
     function loadSettings() {
-        if (localStorage.lastUsedFeedId != 'undefined') {
+        if (localStorage.lastUsedFeedId !== undefined) {
             lastUsedFeedId = parseInt(localStorage.lastUsedFeedId,10);
             maxItemsToShow = localStorage.maxItemsToShow;
         }
